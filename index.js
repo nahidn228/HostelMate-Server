819d10f4f99d014b53602ca0e5539ea31c820351
Nahid Hasan <nahidn228@gmail.com>
2025-01-16 00:34:19 +0600

    create middlewares verify token

--- index.js
+++ index.js
@@ -39,10 +39,23 @@ async function run() {
       res.send({ token });
     });
 
-  
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized Access" });
+      }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: " unauthorized Access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
 
     // User related API
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyToken, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
