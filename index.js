a52e76f0e98b1b392f593c7f9c1627db09db587d
Nahid Hasan <nahidn228@gmail.com>
2025-01-15 23:38:26 +0600

    create admin API

--- index.js
+++ index.js
@@ -53,6 +53,18 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
     //get all published meals
     app.get("/meals", async (req, res) => {
       const filter = req.query.filter;
