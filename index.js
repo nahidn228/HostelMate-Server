75a31f4ec08496cd29c2721427fcf6bde6139c9d
Nahid Hasan <nahidn228@gmail.com>
2025-01-16 09:30:07 +0600

    add middleware in every private api call

--- index.js
+++ index.js
@@ -67,25 +67,30 @@ async function run() {
     };
 
     // User related API
-    app.get("/users", verifyToken, async (req, res) => {
+    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
 
     //Admin APi
-    app.get("/users/admin/:email", verifyToken, async (req, res) => {
-      const email = req.params.email;
-      if (email !== req.decoded.email) {
-        return res.status(403).send({ message: "Forbidden access" });
+    app.get(
+      "/users/admin/:email",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ message: "Forbidden access" });
+        }
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        let admin = false;
+        if (user) {
+          admin = user.role === "admin";
+        }
+        res.send({ admin });
       }
-      const query = { email: email };
-      const user = await usersCollection.findOne(query);
-      let admin = false;
-      if (user) {
-        admin = user.role === "admin";
-      }
-      res.send({ admin });
-    });
+    );
 
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -99,24 +104,29 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/users/:id", async (req, res) => {
+    app.delete("/users/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await usersCollection.deleteOne(query);
       res.send(result);
     });
 
-    app.patch("/users/admin/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const updatedDoc = {
-        $set: {
-          role: "admin",
-        },
-      };
-      const result = await usersCollection.updateOne(filter, updatedDoc);
-      res.send(result);
-    });
+    app.patch(
+      "/users/admin/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+    );
 
     //get all published meals
     app.get("/meals", async (req, res) => {
@@ -145,7 +155,7 @@ async function run() {
     });
 
     // Insert a review
-    app.post("/meals/:id", async (req, res) => {
+    app.post("/meals/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const newReview = req.body;
       const filter = { _id: new ObjectId(id) };
@@ -157,7 +167,7 @@ async function run() {
     });
 
     // Increase like count in single meal data
-    app.patch("/meals/:id", async (req, res) => {
+    app.patch("/meals/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
 
       const filter = { _id: new ObjectId(id) };
@@ -175,7 +185,7 @@ async function run() {
     });
 
     // Increase like count in single upcomingMeal data
-    app.patch("/upcomingMeals/:id", async (req, res) => {
+    app.patch("/upcomingMeals/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const { likes, likedBy } = req.body;
       const filter = { _id: new ObjectId(id) };
@@ -186,7 +196,6 @@ async function run() {
           .status(400)
           .send({ error: "You have already liked this meal." });
       }
-
       // If the user hasn't liked the meal, proceed with updating
       const update = {
         $inc: { likes: 1 },
@@ -197,7 +206,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
