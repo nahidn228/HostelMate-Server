9a7c8f01c3466c37b5a40533689af9dfd942a8f2
Nahid Hasan <nahidn228@gmail.com>
2025-01-18 15:28:26 +0600

    payment method integret

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
@@ -34,6 +33,10 @@ async function run() {
     const reviewCollection = client
       .db("MealCollectionDB")
       .collection("reviews");
+    const cartCollection = client.db("MealCollectionDB").collection("carts");
+    const paymentsCollection = client
+      .db("MealCollectionDB")
+      .collection("payments");
 
     // JWT related API
     app.post("/jwt", async (req, res) => {
@@ -105,6 +108,13 @@ async function run() {
       }
     );
 
+    app.get("/user/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       // insert email if user doesn't exists:
@@ -114,13 +124,6 @@ async function run() {
         return res.send({ message: "user already exists", insertedId: null });
       }
       const result = await usersCollection.insertOne(user);
-      res.send(result);
-    });
-
-    app.get("/user/:email", verifyToken, async (req, res) => {
-      const email = req.params.email;
-      const query = { email: email };
-      const result = await usersCollection.findOne(query);
       res.send(result);
     });
 
@@ -383,6 +386,68 @@ async function run() {
     app.post("/reviews", verifyToken, async (req, res) => {
       const review = req.body;
       const result = await reviewCollection.insertOne(review);
+    });
+
+    /**
+     * ***************************************************************
+     *  Carts Collection
+     * ***************************************************************
+     * */
+
+    app.get("/carts/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    /**
+     * ***************************************************************
+     *  Payment
+     * ***************************************************************
+     * */
+
+    // Payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      // console.log(paymentIntent, "from paymentIntent");
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    app.get("/payments/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      if (email !== req.decoded.email) {
+        return res.status(403).send("forbidden access");
+      }
+      const result = await paymentsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentsCollection.insertOne(payment);
+
+      res.send({ paymentResult });
     });
 
     // Send a ping to confirm a successful connection
